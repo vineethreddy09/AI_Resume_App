10c056d1b9b918ab0e3d79c9f6981d2616edb8bf
VINEETHREDDY MULA <2303a51904@sru.edu.in>
2026-03-10 21:29:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from resume_parser import extract_resume_text
+from matcher import match_skills
+
+st.title("AI Resume Screening System")
+
+uploaded_file = st.file_uploader("Upload Resume", type=["pdf"])
+
+if uploaded_file:
+
+    with open("resume.pdf", "wb") as f:
+        f.write(uploaded_file.read())
+
+    resume_text = extract_resume_text("resume.pdf")
+
+    with open("skills.txt") as f:
+        skills = [s.strip() for s in f.readlines()]
+
+    score, matched, missing = match_skills(resume_text, skills)
+
+    st.subheader(f"Match Score: {score:.2f}%")
+
+    st.write("Matched Skills:", matched)
+    st.write("Missing Skills:", missing)
